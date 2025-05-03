7381ec80f6bf417b7c4eb581eeaa32868f547cea
Lee Sing Chan Yau <jukerhdchan@gmail.com>
2025-05-03 00:53:56 -0700

    name changed.

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Alan Chan",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "software engineer",
     bio: "I develop everything using node.",
